e708d18dd2f12b29c5e26bac2b887b86ce97fb6d
Hubert Pitura <hpitura@gmail.com>
2013-07-27 14:31:40 +0200

    First commit

---
+++ src/com/mackie/HelloAndroid/MyActivity.java
@@ -0,0 +1,115 @@
+package com.mackie.HelloAndroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class MyActivity extends Activity {
+    public static final String TAG = "MyActivity";
+    private TextView textView;
+    private ImageView imageView;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        //Uri uri = intent.getData();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+
+            Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+
+            textView.setText("action: " + action + "\n" +
+                             "type: " + type + "\n" +
+                             "uri: " + imageUri.toString()
+                             );
+
+            //imageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.kertesz, 300, 300));
+            imageView.setImageBitmap(decodeSampledBitmapFromUri(imageUri, 500, 500));
+        }
+    }
+
+    int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;                                                                                                 11
+    }
+
+    Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    Bitmap decodeSampledBitmapFromUri(Uri uri, int reqWidth, int reqHeight) {
+        File image = new File(getRealPathFromURI(uri));
+        Log.i(TAG, "File path: " + getRealPathFromURI(uri));
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(image.getPath(), options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(image.getPath(), options);
+    }
+
+    String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+}
